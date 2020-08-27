11ef57fbe9334707a922232107389e4e7b55900e
TechyCriswiz <67975057+TechyCriswiz@users.noreply.github.com>
2020-08-27 11:21:05 -0500

    Add files via upload

---
+++ Cristian_S_NameBadgeApp/Cristian_S_NameBadgeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cristian_S_NameBadgeApp
+//
+//  Created by Cristian Salazar  on 8/27/20.
+//  Copyright © 2020 Cristian Salazar . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
